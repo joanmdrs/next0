bead6da2e05d0f89e7f97040101e9ba0c9dcd483
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-01-17 20:24:55 -0300

    arrumando os arquivos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
+import {Header} from "./components/header"
+import {Principal} from "./principal"
 
-require('babel-register')({
-    presets: ['env']
-});
-
-module.exports = require('./index.js')
-
-import {Header} from "./header"
-import Principal from "./principal"
 const Home = () => {
     return (
         <div>
